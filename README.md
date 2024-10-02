8bede3bcb106ffd30e02018ef5e262ce7fa4a0e5
inspi-writer <conservedinnovation@gmail.com>
2024-10-02 11:59:05 +0100

    arranged project directory strcture

---
+++ README.md
@@ -0,0 +1,24 @@
+# web3-survivor
+
+Helpers and Code Snippets for the everyday web3 Developer 💪
+
+> EVM, BTC, The general works
+> One could say this is a comprehensive wallet app with extra bells and whistles.
+
+## Gm Anon, Frens 🦸🦸🦸🦸
+
+<!-- todo: Basic Introduction to the differences between EVM blockchains and whatnot -->
+<!-- todo: explain rpcs, basic onboarding stuff -->
+
+<!-- todo:  links to standard docuimentations for the chains supported -->
+
+<!-- todo: direction on how to get to web and nodejs directories in this repository ...basically how to find your way around -->
+
+> ## Let's get cooking 🍳
+
+- createWallet
+- importWallet
+- queryBalance
+- fetchTxHistory
+- sendToken
+- interactWithSmartContracts - Uniswap v2 Router
---
+++ nodejs/rpc.ts
@@ -0,0 +1,23 @@
+export const jsonRPC = {
+  ethereum: "https://mainnet.infura.io/v3/2b1c64c1eeb243e185468576d10a32db",
+  avalanche: "https://api.avax.network/ext/bc/C/rpc",
+  bsc: "https://bsc-dataseed.binance.org/",
+  btc: "https://blockstream.info/api/",
+  fantom: "https://rpcapi.fantom.network",
+  litecoin: "explorer.litecoin.net",
+  polygon: "https://rpc-mainnet.maticvigil.com",
+  solana: "https://api.mainnet-beta.solana.com",
+  tron: "https://api.trongrid.io"
+};
+
+export const testnetJSONRPC = {
+  ethereum: "https://mainnet.infura.io/v3/2b1c64c1eeb243e185468576d10a32db",
+  avalanche: "https://api.avax.network/ext/bc/C/rpc",
+  bsc: "https://bsc-dataseed.binance.org/",
+  btc: "https://blockstream.info/api/",
+  fantom: "https://rpcapi.fantom.network",
+  litecoin: "explorer.litecoin.net",
+  polygon: "https://rpc-mainnet.maticvigil.com",
+  solana: "https://api.mainnet-beta.solana.com",
+  tron: "https://api.trongrid.io"
+};
